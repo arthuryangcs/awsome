1024a4b7fa4b395e63b8000490a4d292fdbc59c1
Arthur Yang <yangyemeng@dingfudata.com>
2017-03-13 14:10:44 +0800

    update vim isntall.sh

--- vim/install.sh
+++ vim/install.sh
@@ -1,4 +1,4 @@
 cp vimrc ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
