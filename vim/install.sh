5604c1d3579e783465df6dfa2e8c88716e981518
Arthur Yang <yangyemeng@dingfudata.com>
2017-03-27 16:18:41 +0800

    fix bug

--- vim/install.sh
+++ vim/install.sh
@@ -2,5 +2,5 @@ cp vimrc ~/.vimrc
 git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-python ~/.vim/bundle/YouCompleteMe/install.sh
+python ~/.vim/bundle/YouCompleteMe/install.py
 
