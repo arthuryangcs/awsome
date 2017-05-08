46572a770a84668a0fc5c9fa2142833367d262a5
yangyemeng <yangyemeng@bytedance.com>
2017-05-08 15:56:02 +0800

    add plugin vim-go

--- vim/install.sh
+++ vim/install.sh
@@ -2,5 +2,5 @@ cp vimrc ~/.vimrc
 git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-python ~/.vim/bundle/YouCompleteMe/install.py
+python ~/.vim/bundle/YouCompleteMe/install.py  --gocode-completer
 
