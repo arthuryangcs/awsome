e46116fae4a20b140082f8b290198c21b6789965
yangyemeng <yangyemeng@bytedance.com>
2017-06-29 16:31:32 +0800

    add ycm_extra_conf.py

--- vim/install.sh
+++ vim/install.sh
@@ -3,4 +3,5 @@ git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 python ~/.vim/bundle/YouCompleteMe/install.py  --gocode-completer
+cp ~/.vim/bundle/YouCompleteMe/third_party/ycmd/examples/.ycm_extra_conf.py ~/
 
