e51453d95f5580a5b67db530ab41636ce6cfb141
杨晔萌 <yangyemeng@bytedance.com>
2017-07-02 11:13:13 +0800

    add  --clang-completer

--- vim/install.sh
+++ vim/install.sh
@@ -2,6 +2,6 @@ cp vimrc ~/.vimrc
 git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-python ~/.vim/bundle/YouCompleteMe/install.py  --gocode-completer
+python ~/.vim/bundle/YouCompleteMe/install.py  --gocode-completer --clang-completer
 cp ~/.vim/bundle/YouCompleteMe/third_party/ycmd/examples/.ycm_extra_conf.py ~/
 
